81d94df0a5f9ff9d4a68b8050b5092de79230f45
Felipe Chung <fchung.dev@gmail.com>
2023-09-20 14:13:40 -0300

    add base styles for workout modal

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,30 @@
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  borderRadius: '12px',
+  boxShadow: 24,
+  p: 4,
+};
+
+export const ModalComponent = ({ open, handleClose, children }) => {
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>{children}</Box>
+      </Modal>
+    </div>
+  );
+};
--- src/pages/Workouts/index.jsx
+++ src/pages/Workouts/index.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
 import './index.css';
+import { ModalComponent } from '../../components/Modal';
 import Button from '@mui/material/Button';
 
 import AddIcon from '@mui/icons-material/Add';
 export const Workouts = () => {
+  const [open, setOpen] = useState(false);
+  const [exerciseName, setExerciseName] = useState('');
+  const [sets, setSets] = useState('');
+  const [reps, setReps] = useState('');
+  const [weight, setWeight] = useState('');
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div className="workoutPage">
       <div className="header">
@@ -10,14 +20,76 @@ export const Workouts = () => {
         <Button
           color="primary"
           variant="contained"
-          onClick={() => console.log('test')}
+          onClick={handleOpen}
           style={{ width: '100px', height: '37px' }}
         >
           <AddIcon />
           <span>Add</span>
         </Button>
       </div>
-      <div>
+      <ModalComponent open={open} handleClose={handleClose}>
+        <form onSubmit={() => console.log('test')}>
+          <div className="inputGroup">
+            <label htmlFor="Exercise Name" className="whiteBackground">
+              Exercise Name
+            </label>
+            <input
+              type="text"
+              id="exerciseName"
+              name="exerciseName"
+              required
+              value={exerciseName}
+              onChange={(event) => setExerciseName(event.target.value)}
+            />
+          </div>
+
+          <div className="inputGroup">
+            <label htmlFor="sets" className="whiteBackground">
+              # of Sets
+            </label>
+            <input
+              type="text"
+              id="sets"
+              name="sets"
+              required
+              value={sets}
+              onChange={(event) => setSets(event.target.value)}
+            />
+          </div>
+
+          <div className="inputGroup">
+            <label htmlFor="reps" className="whiteBackground">
+              # of Reps
+            </label>
+            <input
+              type="text"
+              id="reps"
+              name="reps"
+              required
+              value={reps}
+              onChange={(event) => setReps(event.target.value)}
+            />
+          </div>
+
+          <div className="inputGroup">
+            <label htmlFor="weight" className="whiteBackground">
+              Weight
+            </label>
+            <input
+              type="text"
+              id="weight"
+              name="weight"
+              required
+              value={weight}
+              onChange={(event) => setWeight(event.target.value)}
+            />
+          </div>
+          <div className="buttonsContainer">
+            <button type="submit">Add Workout</button>
+          </div>
+        </form>
+      </ModalComponent>
+      <div className="imgContainer">
         <img
           src="/training.svg"
           alt="Woman working out"
