2aabae20439c95cea6dfcd04dce5f523ab592164
Felipe Chung <fchung.dev@gmail.com>
2023-10-02 11:34:14 -0300

    add apexcharts

--- src/pages/fitnessTracker/index.jsx
+++ src/pages/fitnessTracker/index.jsx
@@ -1,7 +1,64 @@
+import Chart from 'react-apexcharts';
 export const FitnessTracker = () => {
+  const data = {
+    options: {
+      chart: {
+        id: 'basic-bar',
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+      },
+    },
+    series: [
+      {
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+    ],
+  };
+
+  const donutData = {
+    series: [44, 55, 41, 17, 15],
+    options: {
+      chart: {
+        type: 'donut',
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: 'bottom',
+            },
+          },
+        },
+      ],
+    },
+  };
   return (
     <div>
-      <span>teste</span>
+      <Chart
+        options={data.options}
+        series={data.series}
+        type="bar"
+        width="500"
+      />
+
+      <Chart
+        options={data.options}
+        series={data.series}
+        type="line"
+        width="500"
+      />
+
+      <Chart
+        options={donutData.options}
+        series={donutData.series}
+        type="donut"
+      />
     </div>
   );
 };
